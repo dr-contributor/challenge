2aac80e49ef2a61c2da329fe369c7ad5c21881a7
Daniel <dr-contributor@local>
2023-10-22 21:24:51 +0200

    init frontend folder

---
+++ frontend/src/app/components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-title">
+        <Link href="/">
+          <h1>Archimed's Capital Bill</h1>
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/investments">Investments</Link>
+          </li>
+          <li>
+            <Link href="/invoices">Capital Bills</Link>
+          </li>
+          <li>
+            <Link href="/users">Investors</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Header from "./components/layout/Header"
+import "./css/app.css";
+import "./css/header.css";
+
+export default function Home() {
+  return (
+    <Header />
+  )
+}
